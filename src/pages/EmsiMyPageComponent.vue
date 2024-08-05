5fec10998d58b60f7689dc3f5eeff3ceb534ae9f
eunjooNine <eunjoo.nine@gmail.com>
2024-08-05 11:34:28 +0900

    [add]은주 파일

---
+++ src/pages/EmsiMyPageComponent.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="main">
+        <h1> 나의 글 ~~~~~~~~ 페이지</h1>
+        <div><router-link to="expostcreate">교환글쓰기  |</router-link></div>
+        <div><router-link to="shpostcreate">|  나눔글쓰기</router-link></div>
+    </div>
+    
+</template>
+  
+
+<script>
+  
+export default {
+    name: 'EmsiMyPageComponent',
+    data() {
+        return {  }
+    },
+}
+</script>
+  
+
+<style scoped>
+#main{
+    padding-top: 200px;
+    display: flex;
+    justify-content: center;
+    
+}
+
+</style>
+  
