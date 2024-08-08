2754a5d24bd96e2298cec3a44487292bae67029a
choijw1116 <choijw1116@gmail.com>
2024-08-08 09:15:59 +0900

    [fix]  routes 추가 및 삭제

--- router/index.js
+++ router/index.js
@@ -1,35 +1,38 @@
-import {createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 
 import LoginPage from "@/pages/LoginPage.vue";
 import SignupPage from "@/pages/SignupPage.vue";
 import ChatListPage from "@/pages/ChatListPage.vue";
 import ChatRoomPage from "@/pages/ChatRoomPage.vue";
 import MyPage from "@/pages/MyPage.vue";
-import EmsiMyPageComponent from "@/pages/EmsiMyPageComponent.vue";
 import ExPostCreateComponent from "@/pages/ExPostCreateComponent.vue";
 import PoatCreate from "@/pages/PostCreat.vue";
 import WriteCateModal from "@/components/WriteCateModal.vue";
-import ExPostListComponent from "@/pages/ExPostListComponent.vue";
-import ShPostListComponent from "@/pages/ShPostListComponent.vue";
 import ExPostReadComponent from "@/pages/ExPostReadComponent.vue";
 import ShPostReadComponent from "@/pages/ShPostReadComponent.vue";
+import SharePage from "@/pages/SharePage.vue";
+import ExchangePage from "@/pages/ExchangePage.vue";
+import MyPostPage from "@/pages/MyPostPage.vue";
+import MainPage from "@/pages/MainPage.vue";
+
 
 const router = createRouter({
-    history:createWebHistory(),
-    routes:[
-        {path:"/login", component: LoginPage},
-        {path:"/signup", component: SignupPage},
-        {path:"/chat", component: ChatListPage},
-        {name: "chat-room", path:"/chat-room/:roomIdx", component: ChatRoomPage},
-        {path:"/mypage", component: MyPage},
-        {path:"/emsimypage", component: EmsiMyPageComponent},
-        {path:"/expostcreate", component: ExPostCreateComponent},
-        {name: "postcreate", path:"/postcreate/:postType", component: PoatCreate},
-        {path:"/writecate", component: WriteCateModal},
-        {path:"/expostlist", component: ExPostListComponent},
-        {path:"/shpostlist", component: ShPostListComponent},
-        {path:"/expostread", component: ExPostReadComponent},
-        {path:"/shpostread", component: ShPostReadComponent},
+    history: createWebHistory(),
+    routes: [
+        { path: "/login", component: LoginPage },
+        { path: "/", component: MainPage },
+        { path: "/signup", component: SignupPage },
+        { path: "/chat", component: ChatListPage },
+        { name: "chat-room", path: "/chat-room/:roomIdx", component: ChatRoomPage },
+        { path: "/mypage", component: MyPage },
+        { path: "/myposts", component: MyPostPage },
+        { path: "/expostcreate", component: ExPostCreateComponent },
+        { name: "postcreate", path: "/postcreate/:postType", component: PoatCreate },
+        { path: "/writecate", component: WriteCateModal },
+        { path: "/exchange", component: ExchangePage },
+        { path: "/share", component: SharePage },
+        { path: "/expostread", component: ExPostReadComponent },
+        { path: "/shpostread", component: ShPostReadComponent },
     ],
 });
 
