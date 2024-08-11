b53ca28bc94cfa98c93ed812914ebdc95c8d66c8
choijw1116 <choijw1116@gmail.com>
2024-08-11 19:28:29 +0900

    라우터 수정

--- router/index.js
+++ router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 
-import LoginPage from "@/pages/LoginPage.vue";
-import SignupPage from "@/pages/SignupPage.vue";
-import ChatListPage from "@/pages/ChatListPage.vue";
-import ChatRoomPage from "@/pages/ChatRoomPage.vue";
+import LoginPage from "@/pages/member/LoginPage.vue";
+import SignupPage from "@/pages/member/SignupPage.vue";
+import ChatListPage from "@/pages/chat/ChatListPage.vue";
+import ChatRoomPage from "@/pages/chat/ChatRoomPage.vue";
 import MyPage from "@/pages/MyPage.vue";
-import ExPostCreateComponent from "@/pages/ExPostCreateComponent.vue";
-import PoatCreate from "@/pages/PostCreat.vue";
-import WriteCateModal from "@/components/WriteCateModal.vue";
-import ExPostReadPage from "@/pages/ExPostReadPage.vue";
+// import ExPostCreateComponent from "@/pages/ExPostCreateComponent.vue";
+import PoatCreate from "@/pages/post/PostCreat.vue";
+// import WriteCateModal from "@/components/WriteCateModal.vue";
+import ExPostReadPage from "@/pages/post/ExPostReadPage.vue";
 import SharePostReadPage from "@/pages/SharePostReadPage.vue";
-import SharePage from "@/pages/SharePage.vue";
-import ExchangePage from "@/pages/ExchangePage.vue";
-import MyPostPage from "@/pages/MyPostPage.vue";
+import SharePage from "@/pages/post/SharePage.vue";
+import ExchangePage from "@/pages/post/ExchangePage.vue";
+import MyPostPage from "@/pages/post/MyPostPage.vue";
 import MainPage from "@/pages/MainPage.vue";
-import SelectCategory from "@/pages/SelectCategory.vue"
+// import SelectCategory from "@/pages/SelectCategory.vue"
 import RedirectPage from "@/pages/RedirectPage.vue"
 import HomePage from '@/pages/HomePage.vue'
 
@@ -31,16 +31,16 @@ const router = createRouter({
                 { path: "/login", component: LoginPage },
                 { path: "/signup", component: SignupPage },
                 { path: "/chat", component: ChatListPage },
-                { name: "/chat-room", path: "/chat-room/:roomIdx", component: ChatRoomPage },
+                { name: "chat-room", path: "/chat-room/:roomIdx", component: ChatRoomPage },
                 { path: "/mypage", component: MyPage },
                 { path: "/myposts", component: MyPostPage },
                 { name: "postcreate", path: "/postcreate/:postType", component: PoatCreate },
-                { path: "/exchange", component: ExchangePage },
-                { path: "/share", component: SharePage },
-                { path: "/writecate", component: WriteCateModal },
-                { name: "/expostread", path: "/expostread/:postIdx", component: ExPostReadPage },
+                { name: "exchange", path: "/exchange", component: ExchangePage },
+                { name: "share", path: "/share", component: SharePage },
+                // { path: "/writecate", component: WriteCateModal },
                 { name: "expostdetail", path: "/expostread/:id", component: ExPostReadPage },
                 { name: "sharedetail", path: "/sharedetail/:id", component: SharePostReadPage },
+
             ]
         },
         { path: "/redirect", component: RedirectPage },
